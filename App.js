91ae87b455ca910a770e1eade1f2370789b11ee9
bunga <bungasuweca@gmail.com>
2023-03-24 10:36:33 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,42 +1,31 @@
-import {Component, useState} from 'react';
-import {StyleSheet, View, TextInput} from 'react-native';
-import {Button} from '@rneui/themed';
+import {useState} from 'react';
+import {StyleSheet, View, Text, ScrollView, FlatList} from 'react-native';
 
+import TaskInput from './components/TaskItem';
 import TaskItem from './components/TaskItem';
 
 export default function App() {
-  const [enteredTaskText, setEnteredTaskText] = useState('');
   const [MyTask, setMyTask] = useState('');
 
-  function inputTaskHandler(enteredText) {
-    setEnteredTaskText(enteredText);
-  }
-
-  function addTaskBtnHandler() {
+  function addTaskBtnHandler(enteredTaskText) {
     // console.log(enteredTaskText);
-    setMyTask(currentTask => [...currentTask, enteredTaskText]);
+    setMyTask(currentTask => [
+      ...currentTask,
+      {text: enteredTaskText, key: Math.random().toString},
+    ]);
   }
 
   return (
     <View style={styles.container}>
-      <View style={styles.input}>
-        <TextInput
-          style={styles.txtInput}
-          placeholder="What's Today's Task?"
-          onChangeText={inputTaskHandler}
-        />
-        <Button
-          style={styles.btn}
-          title="Add Task"
-          type="outline"
-          onPress={addTaskBtnHandler}
-        />
-      </View>
-
+      <TaskInput onAddTask={addTaskBtnHandler} />
       <View style={styles.listContainer}>
-        <ScrollView>
-          return <TaskItem />;
-        </ScrollView>
+        <FlatList
+          data={MyTask}
+          renderItem={itemData => {
+            return <TaskItem text={itemData.item.text} />;
+          }}
+          alwaysBounceVertical={false}
+        />
       </View>
     </View>
   );
@@ -49,28 +38,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     // backgroundColor: '#EDE9D5',
   },
-  input: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#617143',
-    height: 80,
-  },
-  txtInput: {
-    borderWidth: 1,
-    borderColor: '#cccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8,
-    height: 35,
-  },
+
   listContainer: {
     flex: 2,
   },
   btn: {
     minHeight: 80,
     height: 80,
+  },
+  tasks: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+
+    backgroundColor: '#ECF2FF',
   },
 });
